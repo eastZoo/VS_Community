43b0e0d6c41cf85985b14f8d5b8773a1cca2aaa4
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-07-12 16:08:30 +0900

    feat : add tweet in firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,32 @@
-import React from "react";
+import {addDoc, collection } from "firebase/firestore"
+import { dbService } from "myBase";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+    const [newTweet, setNewTweet] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        // 컬랙션 생성 함수 사용법, 
+        await addDoc(collection(dbService, "newTweet"), {
+            text: newTweet,
+            createdAt: Date.now(),
+          });
+        setNewTweet("");
+    };
+    const onChange = (event) => {
+        const { target: { value },
+    } = event;
+    setNewTweet(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={newTweet} onChange={onChange} type="text" placeholder="너의 새로운 생각을 보여줘." maxLength={120}/>
+                <input type="submit" value="트윗!"/>
+            </form>
+        </div>
+    )
+}
 
 export default Home;
